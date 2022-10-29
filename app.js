e0fd689258c934275f13c0520b5255377e7b9a1c
pj2sharon55@gmail.com <pj2sharon55@gmail.com>
2022-10-29 17:38:20 +0900

    Blog Starts

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const router = require("./routes")
+
+const app = express()
+
+app.use("/", router)
+app.listen(3000)
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const { Router } = require("express")
+const router = Router()
+router.get("/", (req, res, next) => {
+    res.send("Hello world!")
+})
+
+module.exports = router
